fae7ba006de6028f3b9134879b4fd1014a3a4517
robertcode200 <robertcode200@gmail.com>
2026-03-15 23:12:56 +0800

    feat: wire RTK Query into store with api reducer and middleware
    - Add src/app/api.ts: createApi base with fetchBaseQuery, tagTypes for Categories/Foods/Orders
    - Expand store.ts: [api.reducerPath]: api.reducer added to combineReducers
    - Add api.middleware via getDefaultMiddleware().concat() to enable caching and tag invalidation
    - Keep RootState derived from rootReducer to avoid circular reference

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,7 +1,9 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit'
+import { api } from './api'
 import cartReducer from '../features/cart/cartSlice'
 
 const rootReducer = combineReducers({
+  [api.reducerPath]: api.reducer,
   cart: cartReducer,
 })
 
@@ -10,6 +12,7 @@ export type RootState = ReturnType<typeof rootReducer>
 export const setupStore = (preloadedState?: Partial<RootState>) =>
   configureStore({
     reducer: rootReducer,
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(api.middleware),
     preloadedState,
   })
 
